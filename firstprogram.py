1d8db2dc6552ffc31e86bebf91f6fdd339ccf060
Yashika1615 <katariay22@gmail.com>
2024-04-08 22:39:02 +0530

    python

--- firstprogram.py
+++ firstprogram.py
@@ -1,1 +1,32 @@
 print("hello world")
+print("yashika","ejkfc")
+print(23)
+print(25+23)
+
+name="sgsd"
+age=45
+price=45.88
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+print("name")
+print(name)
+
+age2=age
+
+print("my name is: " , name)
+print("my age is: ", age)
+
+print(age2)
+
+name="dh"
+name1='bdjkc'
+name2='''fhjdb'''
+
+age3=23
+old=False
+a=None
+print(type(old))
+print(type(a))
