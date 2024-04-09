217abae6c2a37822eea85449e7866ed50b9624f8
Yashika1615 <katariay22@gmail.com>
2024-04-09 22:05:12 +0530

    ..

---
+++ functions.py
@@ -0,0 +1,16 @@
+def greetHello():
+    print("hello")
+
+def greetHello1(name, ending):
+    print("HELLO")
+    print("HELLO")
+    print("HELLO")
+    print("HELLO")
+    print("HELLO")
+    
+    print("HELLO")
+    
+
+print("function")
+greetHello1("Haeyr","thntg")
+print("done")
--- loops.py
+++ loops.py
@@ -17,3 +17,36 @@ else:
     print("no")
 
 print("end of program")
+
+#match case
+a=int(input("enter no"))
+a=4
+match a:
+    case 1:
+        print("case1")
+    case 2:
+         print("case2")
+    case _:
+        print("default")
+
+#for loop
+for i in range(5):
+   print(i+1)
+
+a=[1,3,4,5,6,7]
+for item in a:
+    print(item)
+
+#while loop
+
+# j=4
+# while(j<10):
+#     print(j)
+#     j=j+1
+# print("program khatam")
+
+
+# while(True):
+#     print("whle loop not terminating")
+
+
