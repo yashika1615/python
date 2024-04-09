7b7dd84f7ad71aaba75adf407356d457ad5e736c
Yashika1615 <katariay22@gmail.com>
2024-04-09 10:24:08 +0530

    ..

--- sum.py
+++ sum.py
@@ -4,3 +4,14 @@ sum=a+b
 diff=a-b
 print(sum)
 print(diff)
+
+#taking input from user and printing it
+name= input("name: ")
+print(name)
+
+age=int(input("age: "))
+price= float(input("float: "))
+
+print("My name is", name, "and my age is :", age)
+
+#variables in python
